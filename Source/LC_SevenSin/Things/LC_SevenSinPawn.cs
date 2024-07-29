37e2d58178fd79d7226707d921bcb0a369096b54
DarthCY-K <452710557@qq.com>
2024-07-29 08:58:28 +0800

    update：跟随框架更新

---
+++ Source/LC_SevenSin/Things/LC_SevenSinPawn.cs
@@ -0,0 +1,12 @@
+﻿using LCAnomalyLibrary.Comp.Abstract;
+
+namespace LC_SevenSin.Things
+{
+    public class LC_SevenSinPawn : LC_EntityBasePawn
+    {
+        public LC_SevenSinPawn() 
+        {
+
+        }
+    }
+}
